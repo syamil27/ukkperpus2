7361f0e754b6ddcd1b890b83cff3ca8f39181956
syamil27 <msyamil207@gmail.com>
2025-01-16 15:26:06 +0700

    initial commit

---
+++ database/migrations/2025_01_09_091918_create_bukus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column:'kategori_id')->constrained(table:'kategori_bukus');
+            $table->string('judul');
+            $table->string('penulis');
+            $table->string('penerbit');
+            $table->string('tahun_terbit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
